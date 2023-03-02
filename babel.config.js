90deaa422fe7d12184204dff414e3480ecc9483b
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-03-02 17:04:15 -0300

    babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // 'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root:['./src'],
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            // '@hooks': './src/hooks',
+            // '@utils': './src/utils',
+            // '@types': './src/types',
+            // '@common': './src/common',
+            // '@assets': './src/assets',
+            '@screens': './src/screens',
+            // '@context': './src/context',
+            // '@services': './src/services',
+          }
+        }
+      ]
+    ],
   };
 };
